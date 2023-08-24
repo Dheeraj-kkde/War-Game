088e82741af5e1b812634430f5ca0d88d1c99c97
Dheeraj Kumar <91405138+Dheeraj-kkde@users.noreply.github.com>
2023-08-24 15:55:33 +0530

    Initial Commit

---
+++ War Game/War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War Game
+//
+//  Created by Dheeraj Kumar on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
